8933582be29beefd9fc6ea767278d8f8be7c417e
Erik Andersson <erik.anderzzon@gmail.com>
2020-11-23 11:41:43 +0100

    Initial Commit

---
+++ GraveFinder/GraveFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraveFinderApp.swift
+//  GraveFinder
+//
+//  Created by Erik Westervind on 2020-11-23.
+//
+
+import SwiftUI
+
+@main
+struct GraveFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
