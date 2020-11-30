9b7a6404e0eac627b43b55faa07206f3541b3eef
Alessio Di Imperio <55179153+alessiodiimperio@users.noreply.github.com>
2020-11-30 09:47:12 +0100

    Autocomplete: Added search on text input. Highlight on selection until new search. (#9)

--- GraveFinder/Views/BottomSheetView.swift
+++ GraveFinder/Views/BottomSheetView.swift
@@ -22,7 +22,7 @@ struct BottomSheetView : View {
             VStack{
                 
                 BottomSheet(viewModel:viewModel,offset: $offset, value: (-reader.frame(in: .global).height + 150))
-                    .offset(y: reader.frame(in: .global).height - 140)
+                    .offset(y: reader.frame(in: .global).height - 150)
                     // adding gesture....
                     .offset(y: offset)
                     .gesture(DragGesture().onChanged({ (value) in
@@ -92,41 +92,51 @@ struct BottomSheet : View {
     @ObservedObject var viewModel : GravesViewModel
     //    @Binding var searchTxt:String
     
-    @State  var txt = ""
-    @State var searchTxt = ""
+    @State  var query = ""
+    @State private var isSearching = false
+    @State private var isAutoCompleting = false
+    @State private var selectedGrave:Grave? = nil
+    
     @Binding var offset : CGFloat
     var value : CGFloat
     
     @State var output: String = ""
     @State var input: String = ""
     @State var typing = false
-    @State private var isSearching = false
-    @State private var currentPage = 1
+    
+    enum ShowContent {
+        case searchResults, noResults, nothing
+    }
+    @State private var showContent = ShowContent.nothing
     
     var body: some View{
-        
         VStack{
-            
             Capsule()
                 .fill(Color.gray.opacity(0.5))
                 .frame(width: 50, height: 5)
                 .padding(.top)
                 .padding(.bottom,5)
-            
             HStack(spacing: 15){
-                
                 Image(systemName: "magnifyingglass")
                     .font(.system(size: 22))
                     .foregroundColor(.gray)
-                
-                TextField("Search...", text: $txt, onEditingChanged: {_ in
+                TextField("Search...", text: $query, onCommit: {
+                    viewModel.currentPage = 1
+                    viewModel.totalGravesList.removeAll()
+                    viewModel.fetchGraves(for: query, at: viewModel.currentPage)
                     
-                }, onCommit: {
-                    viewModel.totalList.removeAll()
-                    self.searchTxt = self.txt
-                    self.isSearching = true
-                    self.viewModel.fetchGraves(for: self.searchTxt, atPage: currentPage)
+                }).onChange(of: query, perform: { _ in
+                    viewModel.currentPage = 1
+                    viewModel.selectedGraves.removeAll()
+                    if(query.count > 0){
+                        viewModel.totalGravesList.removeAll()
+                        viewModel.fetchGraves(for: query, at: viewModel.currentPage)
+                        showContent = .searchResults
+                    } else {
+                        showContent = .noResults
+                    }
                 })
+                
                 //                { (status) in
                 //
                 //                    withAnimation{
@@ -143,34 +153,47 @@ struct BottomSheet : View {
             .cornerRadius(15)
             .padding()
             
-            
-            ScrollView(.vertical, showsIndicators: false, content: {
-                
-                if isSearching {
-                    LazyVStack(alignment: .leading, spacing: 15, content: {
-                        if (viewModel.totalList.count > 0) {
-                            ForEach(viewModel.totalList,id:\.self){ grave in
-                                GraveView(grave: grave).onTapGesture {
-                                    offset = 0
+            ScrollView(.vertical, showsIndicators: true, content: {
+                switch showContent {
+                case .noResults:
+                    Text("No results")
+                        .font(.system(.headline))
+                case .searchResults:
+                    LazyVStack(alignment: .leading, spacing: 5, content:{
+                        ForEach(viewModel.totalGravesList){
+                            grave in
+                            let isSelectedGrave = grave == selectedGrave
+                            AutoCompleteText(for: grave, andHighLightIf: isSelectedGrave)
+                                .foregroundColor(.black)
+                                .onTapGesture {
                                     viewModel.selectedGraves.removeAll()
+                                    selectedGrave = grave
+                                    offset = 0
+                                    
                                     let graveLocation = GraveLocation(name: grave.deceased!, latitude: grave.location.latitude!, longitude: grave.location.longitude!, birth: grave.dateOfBirth ?? "", death: grave.dateOfDeath ?? "")
+                                    
                                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                                         viewModel.selectedGraves.append(graveLocation)
                                         print("Dead person: \(graveLocation) added")
                                     }
+                                    
                                 }
+                        }
+                        LazyHStack(alignment: .center) {
+                            Spacer()
+                            Text("Loading more...").padding().onAppear {
+                                viewModel.currentPage += 1
+                                viewModel.fetchGraves(for: query, at: viewModel.currentPage)
                             }
-                            } else {
-                                Text("No results").font(.system(.headline))
-                            }
-                        
+                            Spacer()
+                        }
                     })
-                    .padding()
-                    .padding(.top)
+                    
+                default:
+                    EmptyView()
                 }
                 
             })
-            
         }
         .background(BlurView(style: .systemMaterial))
         .cornerRadius(15)
@@ -178,18 +201,13 @@ struct BottomSheet : View {
 }
 
 struct BlurView : UIViewRepresentable {
-    
     var style : UIBlurEffect.Style
     
     func makeUIView(context: Context) -> UIVisualEffectView{
-        
         let view = UIVisualEffectView(effect: UIBlurEffect(style: style))
-        
         return view
     }
     
     func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
-        
-        
     }
 }
