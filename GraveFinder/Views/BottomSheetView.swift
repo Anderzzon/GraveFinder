9977a778cfd0bc06d4b8d6385ddad72068d8e44f
Erik Andersson <erik.anderzzon@gmail.com>
2020-11-30 09:58:30 +0100

    Removed duplicated graveLocation declaration

--- GraveFinder/Views/BottomSheetView.swift
+++ GraveFinder/Views/BottomSheetView.swift
@@ -170,8 +170,6 @@ struct BottomSheet : View {
                                     selectedGrave = grave
                                     offset = 0
                                     let graveLocation = viewModel.createGraveLocation(name: grave.deceased!, latitude: grave.location.latitude!, longitude: grave.location.longitude!, birth: grave.dateOfBirth ?? "", death: grave.dateOfDeath ?? "")
-                                    offset = 0
-                                    let graveLocation = viewModel.createGraveLocation(name: grave.deceased!, latitude: grave.location.latitude!, longitude: grave.location.longitude!, birth: grave.dateOfBirth ?? "", death: grave.dateOfDeath ?? "")
                                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                                         viewModel.selectedGraves.append(graveLocation)
                                         print("Dead person: \(graveLocation) added")
