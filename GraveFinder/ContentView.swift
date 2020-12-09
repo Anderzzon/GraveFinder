92a18cb4dd8ff7acc62f39074b746b8a2c2febfd
Alessio Di Imperio <alessio.diimperio@gmail.com>
2020-12-09 14:03:06 +0100

    Added alternate layout for portrait / landscape

--- GraveFinder/ContentView.swift
+++ GraveFinder/ContentView.swift
@@ -10,13 +10,22 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.verticalSizeClass) var verticalSizeClass: UserInterfaceSizeClass?
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass: UserInterfaceSizeClass?
     @ObservedObject var viewModel = GravesViewModel()
+    
     var body: some View {
-        GeometryReader{ geometry in
-            ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom), content: {
-                MapView(viewModel: viewModel)
-                BottomSheet(viewModel: viewModel)
-            })
+        if (horizontalSizeClass == .regular && verticalSizeClass == .compact) || (horizontalSizeClass == .compact && verticalSizeClass == .compact) {
+            //iPhone landscape
+            MapView(viewModel: viewModel)
+        } else {
+            //Other
+            GeometryReader{ geometry in
+                ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom), content: {
+                    MapView(viewModel: viewModel)
+                    BottomSheet(viewModel: viewModel)
+                })
+            }
         }
     }
 }
