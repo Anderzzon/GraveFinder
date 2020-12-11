630e75f30e8b74b0ab2a63b6f4460dd8baef9435
Erik Andersson <erik.anderzzon@gmail.com>
2020-12-11 13:42:43 +0100

    Added NetStatus file with functions

--- GraveFinder/GraveFinderApp.swift
+++ GraveFinder/GraveFinderApp.swift
@@ -10,6 +10,9 @@ import CoreData
 
 @main
 struct GraveFinderApp: App {
+    
+    //@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelagate
+    
     let viewContext = PersistenceController.shared.container.viewContext
 
     var body: some Scene {
@@ -18,3 +21,16 @@ struct GraveFinderApp: App {
         }
     }
 }
+
+//class AppDelegate: NSObject, UIApplicationDelegate  {
+//    private let netStatus = NetStatus.shared
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any ]? = nil ) -> Bool {
+//        netStatus.startMonitoring()
+//
+//        return true
+//    }
+//
+//    func applicationDidEnterBackground(_ application: UIApplication) {
+//        netStatus.stopMonitoring()
+//    }
+//}
