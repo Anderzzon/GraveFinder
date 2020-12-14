ed4f919782127ce061ee4186e4be3c93e1e56107
Shafigh Khalili <shafiqkhalili@gmail.com>
2020-12-14 23:02:41 +0100

    Refactor BottomSheetView to small code blocks (#29)

---
+++ GraveFinder/Views/CustomViews/SearchViewModifier.swift
@@ -0,0 +1,57 @@
+//
+//  SearchViewModifier.swift
+//  GraveFinder
+//
+//  Created by Shafigh Khalili on 2020-12-14.
+//
+
+import SwiftUI
+
+internal extension BottomSheetView {
+
+    func SearchViewModifier(readerHeight:CGFloat) -> some View {
+        HStack(spacing: 15){
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(.gray)
+            TextField("Search...", text: $query,onEditingChanged: {EditMode in
+
+                if(!self.pulledUp){
+                    offset = (-readerHeight + searchBarHeight)
+                    self.pulledUp = true
+                }
+                if(!EditMode){
+                    self.pulledUp = false
+                }
+            }, onCommit: {
+                viewModel.currentPage = 1
+                viewModel.totalGravesList.removeAll()
+                viewModel.fetchGraves(for: query, at: viewModel.currentPage)
+
+            })
+            .onChange(of: query, perform: { _ in
+                self.setOptions(index: 0)
+                if onlyFavorites == 1{
+                    onlyFavorites = 0
+                    showContent = .searchResults
+                }
+                viewModel.currentPage = 1
+                viewModel.selectedGraves.removeAll()
+                if(query.count > 0){
+                    viewModel.totalGravesList.removeAll()
+                    viewModel.fetchGraves(for: query, at: viewModel.currentPage)
+                    showContent = .searchResults
+                } else {
+                    showContent = .nothing
+                }
+            })
+            .disableAutocorrection(true)
+        }
+        .padding()
+        .padding(.horizontal,10)
+        .background(
+            Capsule().fill(Color.gray.opacity(0.2)),
+            alignment: .leading
+        )
+    }
+}
+
