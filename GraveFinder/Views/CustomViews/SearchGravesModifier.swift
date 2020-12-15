17d2ae1762e59b939b47613e9096777143881a29
Shafigh Khalili <shafiqkhalili@gmail.com>
2020-12-15 09:55:41 +0100

    Add missing files

---
+++ GraveFinder/Views/CustomViews/SearchGravesModifier.swift
@@ -0,0 +1,40 @@
+//
+//  ScrollViewModifier.swift
+//  GraveFinder
+//
+//  Created by Shafigh Khalili on 2020-12-14.
+//
+
+import SwiftUI
+
+internal extension BottomSheetView {
+    func SearchGravesModifier() -> some View {
+        LazyVStack(alignment: .leading, spacing: 5, content:{
+            ForEach(viewModel.totalGravesList){
+                grave in
+
+                GravesView(for: grave, selectedGrave: $selectedGrave, disabledIf: !grave.isLocatable(), offset: $offset, viewModel: viewModel)
+            }
+            if viewModel.totalPages > 1 && viewModel.currentPage < viewModel.totalPages {
+                HStack(alignment: .center) {
+                    Spacer()
+                    Button(action: {
+                        viewModel.currentPage += 1
+                        viewModel.fetchGraves(for: query, at: viewModel.currentPage)
+                    }, label: {
+                        Text("Visa fler...")
+                    }).padding(.bottom, 40)
+                    Spacer()
+                }
+            } else {
+                HStack{
+                    Spacer()
+                    Text("Slut på resultat...")
+                        .font(.caption2)
+                        .padding(.bottom, 40)
+                    Spacer()
+                }
+            }
+        })
+    }
+}
---
+++ GraveFinder/Views/CustomViews/ToggleViewModifier.swift
@@ -0,0 +1,45 @@
+//
+//  SwiftUIView.swift
+//  GraveFinder
+//
+//  Created by Shafigh Khalili on 2020-12-14.
+//
+
+import SwiftUI
+
+internal extension BottomSheetView {
+
+    func ToggleViewModifier() -> some View {
+        VStack{
+            HStack(spacing: 10) {
+                ForEach(self.graveOptions.indices, id: \.self) { index in
+                    
+                    Button(action: {setOptions(index: index)}) {
+                        Text(self.graveOptions[index])
+                    }
+                    .padding(EdgeInsets(top: 16, leading: 20, bottom: 16, trailing: 20))
+                    .background(
+                        GeometryReader { geo in
+                            Color.clear.onAppear { self.setFrame(index: index, frame: geo.frame(in: .global)) }
+                        }
+                    )
+                    .foregroundColor(Color.black).font(.caption)
+                }
+            }
+            .background(
+                Capsule().fill(
+                    Color.white.opacity(0.8))
+                    .frame(width: self.frames[self.selectedIndex].width,
+                           height: self.frames[self.selectedIndex].height, alignment: .topLeading)
+                    .offset(x: self.frames[self.selectedIndex].minX - self.frames[0].minX)
+                , alignment: .leading
+            )
+            .background(Capsule().stroke(Color.gray, lineWidth: 0.2))
+        }
+        .background(
+            Capsule().fill(
+                Color.white.opacity(0.4))
+            , alignment: .leading
+        )
+    }
+}
