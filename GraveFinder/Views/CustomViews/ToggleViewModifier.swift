915b506aee2d49bef9c357124975e530553286e5
Alessio Di Imperio <alessio.diimperio@gmail.com>
2020-12-16 13:18:24 +0100

    Added fixed size to button label for toggle

--- GraveFinder/Views/CustomViews/ToggleViewModifier.swift
+++ GraveFinder/Views/CustomViews/ToggleViewModifier.swift
@@ -13,9 +13,8 @@ internal extension BottomSheetView {
         VStack{
             HStack(spacing: 10) {
                 ForEach(self.graveOptions.indices, id: \.self) { index in
-                    
                     Button(action: {setOptions(index: index)}) {
-                        Text(self.graveOptions[index])
+                        Text(self.graveOptions[index]).frame(width: 60, height: 10, alignment: .center)
                     }
                     .padding(EdgeInsets(top: 16, leading: 20, bottom: 16, trailing: 20))
                     .background(
