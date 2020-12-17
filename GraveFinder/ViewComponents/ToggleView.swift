60b0ec5dff5d2a0c6956ca27b1434d67e13447ce
Alessio Di Imperio <alessio.diimperio@gmail.com>
2020-12-17 20:09:17 +0100

    returned section of toggle view to indices loop . bug with toggle not offset

--- GraveFinder/ViewComponents/ToggleView.swift
+++ GraveFinder/ViewComponents/ToggleView.swift
@@ -14,17 +14,20 @@ struct ToggleView:View {
     var body : some View {
         VStack{
             HStack(spacing: 10) {
-                ForEach(viewModel.bottomSheetDisplayOptions, id: \.self) { option in
+                ForEach(viewModel.bottomSheetDisplayOptions.indices, id: \.self) { index in
                     Button(action: {
-                            viewModel.contentToShow(set: option)
-                            hideKeyboard()
+                        viewModel.contentToShow(set: viewModel.bottomSheetDisplayOptions[index])
+                        hideKeyboard()
                     }) {
-                        Text(option.rawValue).frame(width: 60, height: 10, alignment: .center)
+                        Text(viewModel.bottomSheetDisplayOptions[index].rawValue).frame(width: 60, height: 10, alignment: .center)
                     }
                     .padding(EdgeInsets(top: 16, leading: 20, bottom: 16, trailing: 20))
                     .background(
                         GeometryReader { geo in
-                            Color.clear.onAppear { viewModel.setFrame(index: viewModel.bottomSheetDisplayOptions.firstIndex(of: option)!, frame: geo.frame(in: .global))}
+                            Color.clear.onAppear {
+                                viewModel.setFrame(
+                                    index: index,
+                                    frame: geo.frame(in: .global))}
                         }
                     )
                     .foregroundColor(Color.black).font(.caption)
@@ -36,8 +39,7 @@ struct ToggleView:View {
                     .frame(width: viewModel.frames[viewModel.bottomSheetDisplayOptions.firstIndex(of: viewModel.selectedDisplayOption)!].width,
                            height: viewModel.frames[viewModel.bottomSheetDisplayOptions.firstIndex(of: viewModel.selectedDisplayOption)!].height, alignment: .topLeading)
                     .offset(x: viewModel.frames[viewModel.bottomSheetDisplayOptions.firstIndex(of: viewModel.selectedDisplayOption)!].minX - viewModel.frames[0].minX)
-                , alignment: .leading
-            )
+                , alignment: .leading)
             .background(Capsule().stroke(Color.gray, lineWidth: 0.2))
         }
         .background(
@@ -46,4 +48,41 @@ struct ToggleView:View {
             , alignment: .leading
         )
     }
+//    func ToggleViewModifier() -> some View {
+//        VStack{
+//            HStack(spacing: 10) {
+//                ForEach(self.graveOptions.indices, id: \.self) { index in
+//                    Button(action: {
+//                            setOptions(index: index)
+//                            hideKeyboard()
+//                    }) {
+//                        Text(self.graveOptions[index]).frame(width: 60, height: 10, alignment: .center)
+//                    }
+//                    .padding(EdgeInsets(top: 16, leading: 20, bottom: 16, trailing: 20))
+//                    .background(
+//                        GeometryReader { geo in
+//                            Color.clear.onAppear { self.setFrame(index: index, frame: geo.frame(in: .global)) }
+//                        }
+//                    )
+//                    .foregroundColor(Color.black).font(.caption)
+//                }
+//            }
+//            .background(
+//                Capsule().fill(
+//                     Color.white.opacity(0.8)
+//                      )
+//                    .frame(width: self.frames[self.selectedIndex].width,
+//                           height: self.frames[self.selectedIndex].height, alignment: .topLeading)
+    
+//                    .offset(x: .minX - .minX)
+    
+//                , alignment: .leading)
+//            .background(Capsule().stroke(Color.gray, lineWidth: 0.2))
+//        }
+//        .background(
+//            Capsule().fill(
+//                Color.white.opacity(0.4))
+//            , alignment: .leading
+//        )
+//    }
 }
