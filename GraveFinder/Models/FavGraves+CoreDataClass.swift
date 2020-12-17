7f347df9405bdcb13d34c0c92fbc0e733945c3f9
Alessio Di Imperio <55179153+alessiodiimperio@users.noreply.github.com>
2020-12-17 11:38:30 +0100

    Added plot number to model and edited gravesview to inkl birth and death days. (#39)

--- GraveFinder/Models/FavGraves+CoreDataClass.swift
+++ GraveFinder/Models/FavGraves+CoreDataClass.swift
@@ -2,12 +2,13 @@
 //  FavGraves+CoreDataClass.swift
 //  GraveFinder
 //
-//  Created by Alessio on 2020-12-04.
+//  Created by Alessio on 2020-12-17.
 //
 //
 
 import Foundation
 import CoreData
+
 
 public class FavGraves: NSManagedObject {
 
--- GraveFinder/Models/FavGraves+CoreDataProperties.swift
+++ GraveFinder/Models/FavGraves+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  FavGraves+CoreDataProperties.swift
 //  GraveFinder
 //
-//  Created by Alessio on 2020-12-04.
+//  Created by Alessio on 2020-12-17.
 //
 //
 
@@ -16,15 +16,16 @@ extension FavGraves {
         return NSFetchRequest<FavGraves>(entityName: "FavGraves")
     }
 
-    @NSManaged public var deceased: String?
-    @NSManaged public var dateOfDeath: String?
-    @NSManaged public var dateOfBirth: String?
-    @NSManaged public var dateBuried: String?
     @NSManaged public var cemetery: String?
+    @NSManaged public var dateBuried: String?
+    @NSManaged public var dateOfBirth: String?
+    @NSManaged public var dateOfDeath: String?
+    @NSManaged public var deceased: String?
+    @NSManaged public var graveType: String?
     @NSManaged public var id: String?
     @NSManaged public var latitude: Double
     @NSManaged public var longitude: Double
-    @NSManaged public var graveType: String?
+    @NSManaged public var plotNumber: String?
 
 }
 
