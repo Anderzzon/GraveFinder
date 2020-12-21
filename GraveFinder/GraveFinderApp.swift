4309d06596ee6b4856c0aafd7a4c6b5fd33cac28
Alessio Di Imperio <alessio.diimperio@gmail.com>
2020-12-21 15:41:56 +0100

    Encrypt and decrypt with crypto kit

--- GraveFinder/GraveFinderApp.swift
+++ GraveFinder/GraveFinderApp.swift
@@ -15,6 +15,7 @@ struct GraveFinderApp: App {
     @StateObject var notificationDelegate = NotificationDelegate()
     
     let viewContext = PersistenceController.shared.container.viewContext
+    private let compliance = ComplianceViewModel()
     
     var body: some Scene {
         WindowGroup {
@@ -24,6 +25,11 @@ struct GraveFinderApp: App {
                 .environmentObject(netStatus)
                 .onAppear{
                     NotificationService.setDelegate(delegate: notificationDelegate)
+                    
+                    let encrypted = compliance.encrypt(string: "Super duper secret message about top secret secret stuff!!")
+                    print("encrypted: ", encrypted! as Data)
+                    let decrypted = compliance.decrypt(data: encrypted!)
+                    print("decrypted: ", decrypted!)
                 }
         }
     }
